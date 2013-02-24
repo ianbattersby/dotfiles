d0ff47b590ccc4db0982c6328452c7a9b07dd805
ianbattersby <ian.battersby@gmail.com>
2013-02-24 19:31:46 +0000

    Added kotlin aliases.

---
+++ kotlin/aliases.zsh
@@ -0,0 +1,2 @@
+alias kt='/Users/$USER/Library/Application\ Support/IntelliJIdea12/Kotlin/kotlinc/bin/kotlinc-jvm'
+alias ktjs='/Users/$USER/Library/Application\ Support/IntelliJIdea12/Kotlin/kotlinc/bin/kotlinc-jvm'
