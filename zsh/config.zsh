e29c51d6053caff5f52cae86950e4da191936da5
ianbattersby <ian.battersby@gmail.com>
2014-03-26 18:07:17 +0000

    Update Vim, Slate. Add Git Templates, Ctags config.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,10 +39,19 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+bindkey -v
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+bindkey '\' history-incremental-search-backward
+#bindkey -M vicmd '/' history-incremental-search-backward
+#bindkey "^R" history-incremental-search-backward
+#bindkey '\e[3~' delete-char
+#bindkey '^R' history-incremental-search-backward
+
+#bindkey '^[^[[D' backward-word
+#bindkey '^[^[[C' forward-word
+#bindkey '^[[5D' beginning-of-line
+#bindkey '^[[5C' end-of-line
+#bindkey '^[[3~' delete-char
+#bindkey '^[^N' newtab
+#bindkey '^?' backward-delete-char
