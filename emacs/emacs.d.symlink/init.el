8d43a85855db1079f2120b993f7b59a36f576548
ianbattersby <ian.battersby@gmail.com>
2014-11-19 16:45:02 +0000

    Emacs: Evil key-bindings. Add helm. House-keeping.

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -14,7 +14,7 @@
 (menu-bar-mode -1)
 
 (set-face-attribute 'default nil
-    :family "Source Code Pro" :height 145 :weight 'normal)
+    :family "Source Code Pro" :height 185 :weight 'normal)
 (setq-default truncate-lines 1)
 
 (setq c-basic-offset 4)
@@ -57,9 +57,26 @@
 ;;(install-elisp "https://github.com/m2ym/popup-el/raw/master/popup.el")
 
 ;; evil
+(setq-default evil-shift-width 4)
+(setq-default evil-auto-indent t)
 (require 'evil)
     (evil-mode 1)
-(setq evil-shift-width 4)
+
+(define-key evil-normal-state-map (kbd "<SPC> e") 'find-file)
+(define-key evil-normal-state-map (kbd "<SPC> w") 'evil-write)
+
+(define-key evil-normal-state-map (kbd "M-J") 'flycheck-next-error)
+(define-key evil-normal-state-map (kbd "M-K") 'flycheck-previous-error)
+
+(define-key evil-normal-state-map (kbd "<SPC> cc") 'comment-or-uncomment-region-or-line)
+(define-key evil-visual-state-map (kbd "<SPC> cc") 'comment-or-uncomment-region-or-line)
+(define-key evil-normal-state-map (kbd "<SPC> c <SPC>") 'comment-or-uncomment-region-or-line)
+(define-key evil-visual-state-map (kbd "<SPC> c <SPC>") 'comment-or-uncomment-region-or-line)
+(define-key evil-normal-state-map (kbd "<SPC> cn") 'next-error)
+(define-key evil-normal-state-map (kbd "<SPC> cp") 'previous-error)
+(define-key evil-insert-state-map (kbd "<RET>") 'newline-and-indent)
+(define-key evil-normal-state-map (kbd "<SPC> a") 'helm-ag)
+(define-key evil-normal-state-map (kbd "C-p") 'helm-mini)
 
 ;; evil tabs
 (global-evil-tabs-mode t)
@@ -68,9 +85,37 @@
 (require 'powerline)
 (powerline-center-evil-theme)
 
+;; helm
+(require 'helm-config)
+(require 'helm-command)
+(require 'helm-elisp)
+(require 'helm-misc)
+
+(global-set-key (kbd "C-x f") 'helm-for-files)
+(define-key helm-map (kbd "C-j") 'helm-next-line)
+(define-key helm-map (kbd "C-k") 'helm-previous-line)
+
 ;; omnisharp
-(require 'omnisharp)
 (setq omnisharp-company-do-template-completion t)
+(require 'omnisharp)
+
+(evil-define-key 'normal omnisharp-mode-map (kbd "g d") 'omnisharp-go-to-definition)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> b") 'omnisharp-build-in-emacs)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> cf") 'omnisharp-code-format)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> nm") 'omnisharp-rename-interactively)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> fu") 'omnisharp-helm-find-usages)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<M-RET>") 'omnisharp-run-code-action-refactoring)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> ss") 'omnisharp-start-omnisharp-server)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> sp") 'omnisharp-stop-omnisharp-server)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> fi") 'omnisharp-find-implementations)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> x") 'omnisharp-fix-code-issue-at-point)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> fx") 'omnisharp-fix-usings)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<SPC> o") 'omnisharp-auto-complete-overrides)
+
+(define-key evil-normal-state-map (kbd "<SPC> rt") (lambda() (interactive) (omnisharp-unit-test "single")))
+(define-key evil-normal-state-map (kbd "<SPC> rf") (lambda() (interactive) (omnisharp-unit-test "fixture")))
+(define-key evil-normal-state-map (kbd "<SPC> ra") (lambda() (interactive) (omnisharp-unit-test "all")))
+(define-key evil-normal-state-map (kbd "<SPC> rl") 'recompile)
 
 ;; ido
 (require 'ido)
@@ -80,6 +125,7 @@
 (require 'popup)
 
 ;; company-mode
+(setq company-begin-commands '(self-insert-command))
 (require 'company)
 (require 'csharp-mode)
 (defun my-csharp-mode ()
@@ -94,12 +140,14 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-(setq company-begin-commands '(self-insert-command))
 (add-hook 'after-init-hook 'global-flycheck-mode)
   
 (add-to-list 'company-backends 'company-omnisharp)
 (eval-after-load 'company
                    '(add-to-list 'company-backends 'company-omnisharp))
+
+(define-key company-active-map (kbd "C-j") 'company-select-next-or-abort)
+(define-key company-active-map (kbd "C-k") 'company-select-previous-or-abort)
 
 ;; hl-line
 (global-hl-line-mode 1)
@@ -117,12 +165,11 @@
             (set-face-foreground 'mode-line (cdr color))))))
 
 ;; key-chord
-(require 'key-chord)
-(key-chord-mode 1)
 (setq key-chord-one-key-delay 0.2)
 (setq key-chord-two-keys-delay 0.15)
+(require 'key-chord)
+(key-chord-mode 1)
 
-;; key maps
 (key-chord-define evil-insert-state-map "jk"  'evil-normal-state) 
 (key-chord-define evil-replace-state-map "jk"  'evil-normal-state) 
 (key-chord-define evil-insert-state-map "kj"  'evil-normal-state) 
@@ -132,22 +179,6 @@
 ;; (define-key evil-insert-state-map (kbd "j k") 'evil-normal-state)
 
 ;; (define-key evil-insert-state-map (kbd "k j") 'evil-normal-state)
-(define-key evil-normal-state-map (kbd "<SPC> e") 'find-file)
-(define-key evil-normal-state-map (kbd "<SPC> w") 'evil-write)
-
-(define-key evil-normal-state-map (kbd "M-J") 'flycheck-next-error)
-(define-key evil-normal-state-map (kbd "M-K") 'flycheck-previous-error)
-
-(define-key evil-normal-state-map (kbd "<SPC> cc") 'comment-or-uncomment-region-or-line)
-(define-key evil-visual-state-map (kbd "<SPC> cc") 'comment-or-uncomment-region-or-line)
-(define-key evil-normal-state-map (kbd "<SPC> c <SPC>") 'comment-or-uncomment-region-or-line)
-(define-key evil-visual-state-map (kbd "<SPC> c <SPC>") 'comment-or-uncomment-region-or-line)
-(define-key evil-normal-state-map (kbd "<SPC> cn") 'next-error)
-(define-key evil-normal-state-map (kbd "<SPC> cp") 'previous-error)
-(define-key evil-insert-state-map (kbd "<RET>") 'newline-and-indent)
-
-(define-key company-active-map (kbd "C-j") 'company-select-next-or-abort)
-(define-key company-active-map (kbd "C-k") 'company-select-previous-or-abort)
 
 ;; Custom
 (custom-set-variables
@@ -167,7 +198,8 @@
    (quote ~/src/OmniSharpServer/OmniSharp/bin/Debug/OmniSharp\.exe))
  '(custom-enabled-themes (quote (vivid-chalk)))
  '(custom-safe-themes (quote ("b7feeb278a5b7de0a7c8dfc35c95263fb562e0e7f654d34eac811027fb3e913e" default)))
- '(custom-theme-directory "~/.emacs.d/themes/"))
+ '(custom-theme-directory "~/.emacs.d/themes/")
+ '(helm-ag-insert-at-point (quote word)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
